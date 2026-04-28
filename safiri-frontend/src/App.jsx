ddeb96691138a06c11c588b6e4becbc61eb88ad2
Cynthia Mugo <cynthiamugo25@gmail.com>
2026-04-28 20:33:37 +0300

    Add react-router-dom dependency and clean up App component

--- safiri-frontend/src/App.jsx
+++ safiri-frontend/src/App.jsx
@@ -9,11 +9,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <h1 class="text-3xl font-bold underline">
-    Safiri Gems Website
-      </h1>
-      </div>
       <Home />
     </>
   )
